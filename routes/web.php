1f14c3023cff46a10882e2bf8183d90ff0db539e
Vladyslava24 <vlada.lada.17@gmail.com>
2020-05-18 22:06:01 +0300

    Basic routes, blades and basic authorization

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Basic routes
+ */
+Route::get('/', function () {
+    return view('landing');
+})->name('landing');
+
+Route::get('/tour', function () {
+    return view('container_tour');
+})->name('tour');
+
+Route::get('/one-tour', function () {
+    return view('tour');
+})->name('one-tour');
+
+Route::get('/cruise', function () {
+    return view('container_cruise');
+})->name('cruise');
+
+Route::get('/sale', function () {
+    return view('desc_sale');
+})->name('sale');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/all_applications', function () {
+    return view('all_applications');
+})->name('all_application');
+
+
+/**
+ * Authentification routes
+ */
+Auth::routes([
+    'reset' => false,
+    'verify'=> false
+]);
+
+Auth::routes();
+
+Route::get('/all_applications', 'HomeController@index')->name('home');
+
+// Маршруты аутентификации...
+/*Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');*/
+//Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('/logout', function(){
+    Auth::logout();
+    return Redirect::to('/');
+});
+
+// Маршруты регистрации...
+/*Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');*/
